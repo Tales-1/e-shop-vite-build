fdeef7d962a7af29fcfc6cea3b861cd3a0112ed4
Tales-1 <j.naz248@outlook.com>
2023-02-08 14:16:47 +0000

    Restyled search bar

--- src/components/header/header-items/SearchBar.tsx
+++ src/components/header/header-items/SearchBar.tsx
@@ -46,8 +46,8 @@ const SearchBar:React.FC = () => {
                 <button type="submit" 
                         className="ml-2 px-2 text-sm font-medium text-white
                                 bg-sauvignon-cr rounded-xl border hover:bg-light-p 
-                                focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700
-                                dark:focus:ring-blue-800"
+                                focus:ring-4 focus:outline-none focus:ring-blue-300
+                                "
                         onClick={(e)=>searchItems(e)}>
                             <svg className="w-6 h-6" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                              <span className="sr-only">Search</span>
