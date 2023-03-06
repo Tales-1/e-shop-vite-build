87fb5f88df0561b31b6ae02bcbed43550a6aa373
Tales-1 <Jawad.nazir124@gmail.com>
2023-03-06 12:38:55 +0000

    removed 3x3 grid layout and set a fixed viewport height for hero

--- src/components/landing-page/sections/Hero.tsx
+++ src/components/landing-page/sections/Hero.tsx
@@ -9,8 +9,8 @@ import Button from "components/Button";
 const Hero: React.FC = () => {
   return (
     <>
-      <div className="relative grid grid-rows-3 grid-cols-3 h-[85vh] bg-slate-100 shadow-lg">
-        <div className="grid row-start-1 row-end-4 col-start-1 col-end-4  w-[95%] h-[90%] m-auto relative z-0 
+      <div className="relative grid grid-rows-1 bg-slate-100 shadow-lg h-[80vh]">
+        <div className="grid row-start-1 col-start-1 w-[95%] h-[90%] m-auto relative z-0 
         before:absolute before:inset-0 before:bg-black before:opacity-60 overflow-hidden">
           <img
             className="w-full object-cover h-full"
@@ -19,18 +19,18 @@ const Hero: React.FC = () => {
           />
         </div>
         <div
-          className="text-center col-start-1 col-span-3 row-start-2 m-auto w-3/5 max-w-[60rem] flex flex-col gap-8 
+          className="text-center col-start-1 row-start-1 m-auto w-3/5 max-w-[60rem] flex flex-col gap-8 
                 md:gap-9 items-center text-white z-0"
         >
-          <h1 className="text-5xl sm:text-7xl leading-tight xl:text-7xl font-serif font-bold">
+          <h1 className="text-5xl sm:text-6xl leading-tight xl:text-7xl font-serif font-bold">
             Traditional and Modern Styles To Suit All
-            <span className="block text-2xl md:text-4xl mt-5 font-normal">
+            <span className="block text-2xl lg:text-3xl xl:text-4xl mt-5 font-normal">
               Browse through our collection
             </span>
           </h1>
 
           <Link to="/collection/kurtas">
-            <Button styles="text-lg py-3 px-4 md:px-6 md:py-5 md:text-2xl mt-8 bg-red font-bold">
+            <Button styles="text-base py-2 px-3 lg:py-3 lg:px-5 xl:text-2xl xl:py-4 xl:px-6 bg-red font-bold xl:mt-6">
               SHOP NOW
             </Button>
           </Link>
